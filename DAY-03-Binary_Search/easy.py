4d11c9fc512ba6bd6cda8c5386ac3f6fe026571b
Mohammed Abbas <mohammadabbas50605@gmail.com>
2026-07-15 15:59:15 +0530

    Describe what you changed

---
+++ DAY-03-Binary_Search/easy.py
@@ -0,0 +1,20 @@
+arr = [2, 5, 8, 12, 15, 20, 25]
+target = 15
+
+left = 0
+right = len(arr) - 1
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if arr[mid] == target:
+        print("Found at index", mid)
+        break
+
+    elif arr[mid] < target:
+        left = mid + 1
+
+    else:
+        right = mid - 1
+else:
+    print("Not Found")  
